a63684fb2ed22b8fc37b99ee1b60466685b428af
moh <you@example.com>
2021-01-01 22:28:49 +0300

    include folder added

---
+++ include/env_tools.h
@@ -0,0 +1,17 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   env_tools.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sbashir <mi243@ya.ru>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/12/21 17:56:00 by sbashir           #+#    #+#             */
+/*   Updated: 2020/12/22 17:02:41 by sbashir          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int init_environ();
+char *ft_getenv(const char *name);
+int  ft_unsetenv(const char *name);
+char *ft_putenv(const char *string);
+char *ft_setenv(const char *name, const char *value);
