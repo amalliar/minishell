8700b2ee82358fbe0d8dc179a7e5e1120b88bddd
moh <you@example.com>
2021-01-06 14:05:57 +0300

    one pipe line with multiple pipes are working

--- src/process.c
+++ src/process.c
@@ -27,57 +27,88 @@
 static int g_prev_pipe;
 extern char **environ;
 
+
+#define ERR_TEST \
+do                 {\
+if (!errno)\
+fprintf(stderr,"ERROR: %s %s:%d function:%s\n", strerror(errno),__FILE__, __LINE__, __FUNCTION__ );\
+} while (0)
+
 void child(const t_command *command, int *pipefd)
 {
-	dup2(g_prev_pipe, 0); //TODO:  fd leak? g_pipe == 0?!
-	g_prev_pipe = 0;
+	if(g_prev_pipe)
+		dup2(g_prev_pipe, 0); //TODO:  fd leak? g_pipe == 0?!
 	if(command->pipe)
 	{
-		g_prev_pipe = pipefd[1];
+		close(pipefd[0]);
 		dup2(pipefd[1], 1);
+		ERR_TEST;
 	}
 	execve(command->name, command->params, environ);
-	printf("CHILD: %s\n", strerror(errno));
-	printf("HIT\n");
+	ERR_TEST;
+	exit(42);
 }
 
-void process_one(const t_command *command)
+void handle(int val)
 {
-	pid_t pid;
-	int status;
-	int pipefd[2];
-	if (command->pipe)
-		if (pipe(pipefd) == -1) {
-			perror("pipe");
-			exit(EXIT_FAILURE);
-		}
-	pid = fork();
-
-	if (pid == -1)
-	{
-		perror("fork"); /* произошла ошибка */
-		exit(1); /*выход из родительского процесса*/
-	} else if (pid == 0) // Потомок
-	{
-		printf("CHILD");
-		child(command, pipefd);
-		exit(42);
-	} else //предок
-	{
-		printf("PAR");
-		if(!command->pipe)
-			wait(&status);
-		if (!errno)
-			printf("ERROR: %s\n", strerror(errno));
-	}
+	ERR_TEST;
+	fprintf(stderr, "handle_func!!!\n");
+	exit(val);
 }
 
 int process(const t_list *commands)
 {
+	//signal(SIGPIPE, handle);
+	signal(SIGINT, handle);
+	signal(SIGSTOP, handle);
+	pid_t pid;
+	pid_t pids[] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+
+	int status;
+	int pipefd[2];
+	t_command *command;
+
+	//save_std();
+	int pid_it = 0;
 	while(commands)
 	{
-		process_one(commands->content);
+		command = commands->content;
+		if (command->pipe)
+			if (pipe(pipefd) == -1)
+			{
+				perror("pipe");
+				exit(EXIT_FAILURE);
+			}
+		pid = fork();
+
+		if (pid == -1)
+		{
+			perror("fork"); /* произошла ошибка */
+			exit(1); /*выход из родительского процесса*/
+		} else if (pid == 0) // Потомок
+		{
+			child(command, pipefd);
+		} else //предок
+		{
+			if(g_prev_pipe)
+				close(g_prev_pipe);
+			g_prev_pipe = 0;
+			pids[pid_it++ ] = pid;
+			if (command->pipe)
+			{
+				close(pipefd[1]);
+				g_prev_pipe = pipefd[0];
+			}
+			else
+			{
+				for (int i = 0;i < pid_it;i++)
+				{
+					waitpid(pids[i], &status, 0);
+				}
+			}
+		}
 		commands = commands->next;
 	}
+	return 1;
 }
 
