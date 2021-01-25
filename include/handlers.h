6880566a436b48b2a8c010a566cf1c2fdb2b2e21
amalliar <amalliar@student.21-school.ru>
2021-01-25 18:29:25 +0300

    More Norm changes.

--- include/handlers.h
+++ include/handlers.h
@@ -6,16 +6,16 @@
 /*   By: amalliar <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/24 10:30:56 by amalliar          #+#    #+#             */
-/*   Updated: 2021/01/24 10:31:52 by amalliar         ###   ########.fr       */
+/*   Updated: 2021/01/25 18:26:32 by amalliar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef HANDLERS_H
 # define HANDLERS_H
 
-void 	sigint_h(int n);
-void 	pass(int n);
-void	set_default_signals();
-void 	set_bash_signals();
+void		sigint_h(int n);
+void		pass(int n);
+void		set_default_signals();
+void		set_bash_signals();
 
 #endif
