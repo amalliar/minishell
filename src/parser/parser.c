3d77ac19b11dabfa3914b0dac68de7e20924aa99
amalliar <amalliar@student.21-school.ru>
2021-01-25 20:45:18 +0300

    Fix 'unexpected token' error messages.

--- src/parser/parser.c
+++ src/parser/parser.c
@@ -6,7 +6,7 @@
 /*   By: amalliar <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/15 06:23:19 by amalliar          #+#    #+#             */
-/*   Updated: 2021/01/25 18:30:22 by amalliar         ###   ########.fr       */
+/*   Updated: 2021/01/25 20:41:44 by amalliar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,9 +41,9 @@ t_list			*parser_proc(t_token *token_list)
 			proc_ps_get_stdout(&parser);
 		else if (parser.state == PS_GET_STDOUT_APPEND)
 			proc_ps_get_stdout_append(&parser);
-		else if (parser.state == PS_RETURN_RES)
+		if (parser.state == PS_RETURN_RES)
 			return (parser.cmd_list);
-		else if (parser.state == PS_PARSE_ERROR)
+		if (parser.state == PS_PARSE_ERROR)
 			break ;
 		parser.tok_current = parser.tok_current->next;
 	}
